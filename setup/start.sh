e8ad7aa4f5de7a8bd0180d8997922f38d50bd52f
wnuke <wnuke@wnuke.dev>
2020-07-18 19:05:36 +0200

    update api mod and make minecraft logs go to the instance folder

--- setup/start.sh
+++ setup/start.sh
@@ -1,6 +1,7 @@
 bash /srv/setup/setup.sh
 echo "Launching Minecraft..."
-python3 /srv/setup/launch_mc.py \
+cd /srv/instance && \
+  python3 /srv/setup/launch_mc.py \
     --username="$USERNAME" \
     --password="$PASSWORD" \
     --version="fabric-1.16"
