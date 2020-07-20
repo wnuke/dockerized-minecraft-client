f375e021c9081ae52479c9ce92bb623cb8eeca33
wnuke <wnuke@wnuke.dev>
2020-07-20 10:13:33 +0200

    make script generate docker compose

--- installbots.sh
+++ installbots.sh
@@ -1,9 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-export NUMBEROFBOTS=$1
+NUMBEROFBOTS=$1
 if [ -z $NUMBEROFBOTS ]; then
   NUMBEROFBOTS=1
 fi
+
+BOTCOMPTEMPLATE='
+  BOTNAME:
+    image: dockermcbot:latest
+    volumes:
+      - minecraft:/srv/minecraft
+    deploy:
+      mode: global
+    ports:
+      - BOTPORT:8000
+'
 
 git submodule update --init && \
   cd setup || (echo "Setup directory does not exist, exiting..." && exit); \
@@ -13,4 +24,17 @@ git submodule update --init && \
   cd ../bot || (echo "Bot directory does not exist, exiting..." && exit); \
   docker build . -t dockermcbot:latest && \
   cd .. && \
+  PORT=10000
+  cat docker-compose-template.yml > docker-compose.yml
+  for i in $(seq 0 $NUMBEROFBOTS);
+  do
+  PORT=$((PORT + 1))
+  BOTCOMP=${BOTCOMPTEMPLATE//BOTNAME/mcbot$i}
+  BOTCOMP=${BOTCOMP//BOTPORT/$PORT}
+  echo $BOTCOMP >> docker-compose.yml
+  done && \
+  echo '
+volumes:
+  minecraft:
+    external: true' >> docker-compose.yml && \
   docker-compose up
