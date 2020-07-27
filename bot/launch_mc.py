ca59a827683bd0ad9376a48ff0f3c4f369ca06e1
wnuke <wnuke@wnuke.dev>
2020-07-27 17:22:43 +0200

    optimize dockerfile and use java 14

--- bot/launch_mc.py
+++ bot/launch_mc.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python3
-import minecraft_launcher_lib
-import subprocess
 import argparse
 import random
 import string
+import subprocess
 import uuid
+
+import minecraft_launcher_lib
+
 
 def get_random_string(length):
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
 
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--gamedir",default="/srv/minecraft",help="Directory where the game files are located")
-parser.add_argument("--instdir",default="/srv/instance",help="Directory where the instance config is stored")
-parser.add_argument("--version",default="fabric-1.16",help="The Minecraft version")
-parser.add_argument("--username",default="",help="Mojang username")
-parser.add_argument("--password",default="",help="Mojang password")
+parser.add_argument("--gamedir", default="/srv/minecraft", help="Directory where the game files are located")
+parser.add_argument("--instdir", default="/srv/instance", help="Directory where the instance config is stored")
+parser.add_argument("--version", default="fabric-1.16", help="The Minecraft version")
+parser.add_argument("--username", default="", help="Mojang username")
+parser.add_argument("--password", default="", help="Mojang password")
 args = parser.parse_args().__dict__
 
 username = get_random_string(8)
 uuid = str(uuid.uuid3(uuid.NAMESPACE_DNS, username))
 token = "abcdefghijklmnopqrstuvwxyz"
 
-if not(args["username"] == ""):
-    if not(args["password"] == ""):
-        login_data = minecraft_launcher_lib.account.login_user(args["username"],args["password"])
-        if not("errorMessage" in login_data):
+if not (args["username"] == ""):
+    if not (args["password"] == ""):
+        login_data = minecraft_launcher_lib.account.login_user(args["username"], args["password"])
+        if not ("errorMessage" in login_data):
             username = login_data["selectedProfile"]["name"]
             uuid = login_data["selectedProfile"]["id"]
             token = login_data["accessToken"]
-
 
 options = {
     "username": username,
@@ -40,11 +42,14 @@ options = {
     "launcherVersion": "1.0",
     "demo": "false",
     "gameDirectory": str(args["instdir"]),
-    "jvmArguments": ["-Xms850M", "-Xmx850M"],
+    "jvmArguments": ["-Xms850M", "-Xmx850M", "-XX:+UnlockExperimentalVMOptions", "-XX:+AlwaysPreTouch", "-XX"
+                     ":+UseAdaptiveGCBoundary", "-XX:+UseGCOverheadLimit", "-XX:MaxHeapFreeRatio=80",
+                     "-XX:MinHeapFreeRatio=40", "-XX:-UseG1GC", "-XX:+UseZGC", "-XX:+DisableExplicitGC",
+                     "-XX:-UseParallelGC", "-XX:-UseParallelOldGC"],
     "resolutionWidth": "20",
     "resolutionHeight": "20",
 }
 
-command = minecraft_launcher_lib.command.get_minecraft_command(str(args["version"]),str(args["gamedir"]),options)
+command = minecraft_launcher_lib.command.get_minecraft_command(str(args["version"]), str(args["gamedir"]), options)
 
 subprocess.call(command)
