36a3bd8a434aedeab6f2c942b4ad2ac86f88f056
wnuke <wnuke@wnuke.dev>
2020-08-18 18:13:53 +0200

    update submodules for session changes

--- bot/launch_mc.py
+++ bot/launch_mc.py
@@ -7,13 +7,6 @@ import uuid
 
 import minecraft_launcher_lib
 
-
-def get_random_string(length):
-    letters = string.ascii_lowercase
-    result_str = ''.join(random.choice(letters) for i in range(length))
-    return result_str
-
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--gamedir", default="/srv/minecraft", help="Directory where the game files are located")
 parser.add_argument("--instdir", default="/srv/instance", help="Directory where the instance config is stored")
@@ -22,22 +15,10 @@ parser.add_argument("--username", default="", help="Mojang username")
 parser.add_argument("--password", default="", help="Mojang password")
 args = parser.parse_args().__dict__
 
-username = "nukebot"
-uuid = str(uuid.uuid3(uuid.NAMESPACE_DNS, username))
-token = "abcdefghijklmnopqrstuvwxyz"
-
-if not (args["username"] == ""):
-    if not (args["password"] == ""):
-        login_data = minecraft_launcher_lib.account.login_user(args["username"], args["password"])
-        if not ("errorMessage" in login_data):
-            username = login_data["selectedProfile"]["name"]
-            uuid = login_data["selectedProfile"]["id"]
-            token = login_data["accessToken"]
-
 options = {
-    "username": username,
-    "uuid": uuid,
-    "token": token,
+    "username": "nukebot",
+    "uuid": "0",
+    "token": "0",
     "launcherName": "mclauncher-cmd",
     "launcherVersion": "1.0",
     "demo": "false",
